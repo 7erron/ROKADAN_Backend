7b1e2a665f55ec54dd721295e852c17d8763da5d
Roderick Muñoz <roderick.munozg@gmail.com>
2025-04-13 20:57:39 -0400

    Render

--- config/db.js
+++ config/db.js
@@ -7,7 +7,9 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 // Manejar eventos de error en el pool
