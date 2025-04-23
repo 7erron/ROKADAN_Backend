88613ed33ad27e99ba1611ad6bf7c4695380af0a
Roderick Muñoz <roderick.munozg@gmail.com>
2025-04-23 00:18:33 -0400

    reservas2

--- routes/serviciosRoutes.js
+++ routes/serviciosRoutes.js
@@ -6,28 +6,28 @@ const { validarServicio, validarId } = require('../middlewares/validators');
 
 // Rutas para servicios
 router.route('/')
-  .get(serviciosController.obtenerServicios)
+  .get(serviciosController.obtenerServicios) // Correctamente asignamos el controlador
   .post(
-    verificarToken,
-    verificarRol('admin'),
-    validarServicio,
-    serviciosController.crearServicio
+    verificarToken, // Verifica el token para la autenticación
+    verificarRol('admin'), // Verifica que el rol sea 'admin'
+    validarServicio, // Valida los datos del servicio
+    serviciosController.crearServicio // Asigna la función que crea un servicio
   );
 
 router.route('/:id')
-  .get(validarId, serviciosController.obtenerServicio)
+  .get(validarId, serviciosController.obtenerServicio) // Validación de ID y obtener servicio por ID
   .patch(
-    verificarToken,
-    verificarRol('admin'),
-    validarId,
-    validarServicio,
-    serviciosController.actualizarServicio
+    verificarToken, // Verifica el token para la autenticación
+    verificarRol('admin'), // Verifica que el rol sea 'admin'
+    validarId, // Valida el ID del servicio
+    validarServicio, // Valida los datos del servicio
+    serviciosController.actualizarServicio // Asigna la función que actualiza un servicio
   )
   .delete(
-    verificarToken,
-    verificarRol('admin'),
-    validarId,
-    serviciosController.eliminarServicio
+    verificarToken, // Verifica el token para la autenticación
+    verificarRol('admin'), // Verifica que el rol sea 'admin'
+    validarId, // Valida el ID del servicio
+    serviciosController.eliminarServicio // Asigna la función que elimina un servicio
   );
 
 module.exports = router;
